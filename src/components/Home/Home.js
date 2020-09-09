55c98f4e711e7b459777e6611227b639d33000f1
Eliezer <eliezeresquer21@gmail.com>
2020-09-08 21:41:40 -0600

    feat: gradient Banner in home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import "./Home.css"
 
 function Home() {
   return (
     <div className="home">
-      <img src="https://images-na.ssl-images-amazon.com/images/G/33/kindle/journeys/NDg3YjhkYzYt/NDg3YjhkYzYt-OGU2NDViNGQt-w1500._CB410275740_.jpg" alt="Amazon Prime Image"/>
+      <div className="home__container">
+        <img className="home__image"
+        src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+        alt="Amazon Prime Image"
+        />
+        <div className="home__row">
+          {/*Product */}
+          {/*Product */}
+        </div>
+        <div className="home__row">
+          {/*Product */}
+          {/*Product */}
+          {/*Product */}
+        </div>
+        <div className="home__row">
+          {/*Product */}
+        </div>
+      </div>
+     
     </div>
   )
 }
