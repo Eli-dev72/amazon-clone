1a126b00f0255ffd4564ffc136969141f063e2be
Eliezer <eliezeresquer21@gmail.com>
2020-09-09 21:16:39 -0600

    feat: Home page

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
-import React from 'react';
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-import Header from './components/Header/Header';
-import Home from './components/Home/Home'
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Checkout from "./components/Checkout/Checkout";
 
 function App() {
-  return (
-    
-    <Router>
-      <div className="app">
-      <Switch>
-        <Route path="/checkout">
-        <Header/>
-          <h1>Checkout</h1>
-        </Route>
-        <Route path="/login">
-          <h1>Login</h1>
-        </Route>
-        <Route path="/">
-          <Header/>
-          <Home/>
-        </Route>
-      </Switch>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className="app">
+				<Switch>
+					<Route path="/checkout">
+						<Header />
+						<Checkout />
+					</Route>
+					<Route path="/login">
+						<h1>Login</h1>
+					</Route>
+					<Route path="/">
+						<Header />
+						<Home />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
